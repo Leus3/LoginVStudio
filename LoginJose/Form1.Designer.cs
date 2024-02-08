0ea8e032b0910ebdb7257cd2888d4ecc1f89bf0f
JaimeRodRos <jarodriguez@itpfp.com>
2024-02-08 12:26:35 +0100

    Commit2

--- LoginJose/Form1.Designer.cs
+++ LoginJose/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace LoginJose
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -52,11 +53,21 @@ namespace LoginJose
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(727, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 67);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1448, 673);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -70,6 +81,7 @@ namespace LoginJose
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
